37a284b090bf413aad3ede0d8861811f7d31b4f9
Akagi201 <akagi201@gmail.com>
2015-06-14 11:27:32 +0800

    add ios playground

---
+++ swift/ios/iosswift/iosswift/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var label1 = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+
+label1.backgroundColor = UIColor.redColor()
+label1.text = "Hello"
+label1.textColor = UIColor.blueColor()
+
+/**
+优点: 可视化, 方便 所见即所得
+缺点: 不够成熟
+
+Mind, Debug, Test, 编码实现我们的一些设计猜想
+**/
--- swift/mac/OSXMyPlayground.playground/Contents.swift
+++ swift/mac/OSXMyPlayground.playground/Contents.swift
@@ -9,3 +9,18 @@ print("hello Akagi201")
 let count = 3
 
 print("count \(count)")
+
+var count1 = 13
+
+count1 = 23
+
+let count2 = 10
+
+var count3 = count1 + count2
+
+print(count3)
+
+// 循环
+for i in 0...200 {
+    sin(Double(count2 * i))
+}
