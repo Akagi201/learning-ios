bbae5849846c8c2ef3b52b231ea6da970044f200
Akagi201 <akagi201@gmail.com>
2015-06-14 11:42:10 +0800

    contant and variable practice

--- swift/mac/macswift/macswift/main.swift
+++ swift/mac/macswift/macswift/main.swift
@@ -10,3 +10,29 @@ import Foundation
 
 print("Hello, World!")
 
+// 定义常量和变量
+
+let name = "Akagi201"
+
+print(name)
+
+var newName = "Akagi201刘博"
+
+print(newName)
+
+// 类型指定
+var age:Double = 30
+
+print(age)
+
+// UTF8
+var 刘博 = "hello"
+刘博 = "😄"
+print(刘博)
+
+// 变量打印的时候叠加
+print("hello" + 刘博)
+
+print("hello\(age)")
+
+print("hello" + String(age))
