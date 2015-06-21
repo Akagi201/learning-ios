b14e0dd0998095a640759a36ac423044d9cb26a2
Akagi201 <akagi201@gmail.com>
2015-06-21 16:13:07 +0800

    update ios helloworld demo

--- swift/ios/Helloworld/Helloworld/ViewController.swift
+++ swift/ios/Helloworld/Helloworld/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label1: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        label1.text = "你好"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func button1Click(sender: AnyObject) {
+        label1.text = "你好 Akagi201"
+        label1.backgroundColor = UIColor.redColor()
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
