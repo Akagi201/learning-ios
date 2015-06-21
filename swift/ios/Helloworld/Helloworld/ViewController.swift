300b51a2861f550c119576643ab309b4f86d2d41
Akagi201 <akagi201@gmail.com>
2015-06-21 23:04:31 +0800

    update viewController life cycle

--- swift/ios/Helloworld/Helloworld/ViewController.swift
+++ swift/ios/Helloworld/Helloworld/ViewController.swift
@@ -14,15 +14,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         label1.text = "你好"
+        NSLog("viewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        NSLog("viewWillAppear")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        NSLog("viewDidAppear")
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSLog("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        NSLog("viewDidDisappear")
     }
 
     @IBAction func button1Click(sender: AnyObject) {
         label1.text = "你好 Akagi201"
         label1.backgroundColor = UIColor.redColor()
+        NSLog("button1Click")
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        NSLog("didReceiveMemoryWarning")
         // Dispose of any resources that can be recreated.
     }
 
