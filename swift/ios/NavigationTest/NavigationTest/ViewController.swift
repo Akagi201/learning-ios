50828ec323abf920fb901001656bb592af4abe48
Akagi201 <akagi201@gmail.com>
2015-06-21 21:48:04 +0800

    ad Navigationtest

---
+++ swift/ios/NavigationTest/NavigationTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NavigationTest
+//
+//  Created by Bob Liu on 6/21/15.
+//  Copyright © 2015 Akagi201. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func button1Click(sender: AnyObject) {
+//        self.navigationController?.pushViewController(<#T##viewController: UIViewController##UIViewController#>, animated: <#T##Bool#>)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
