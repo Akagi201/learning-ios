e2c3aa67b8f505b588a36db759cc9145537891bc
Akagi201 <akagi201@gmail.com>
2015-07-15 15:29:48 +0800

    update ostest

---
+++ oc/osx/OSTest/OSTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OSTest
+//
+//  Created by Bob Liu on 7/14/15.
+//  Copyright © 2015 Akagi201. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
